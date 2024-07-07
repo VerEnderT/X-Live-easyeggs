0cf9741cd9306f31373cb2d0d412a4efd717bb44
VerLinuxT <53666253+VerEnderT@users.noreply.github.com>
2024-07-07 18:16:52 +0200

    Delete usr/share/easyeggs directory

--- usr/share/easyeggs/clean_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#sudo find / -name penguins-eggs.desktop -delete 2>/dev/null
-
-echo -n > ~/.local/share/recently-used.xbel
-echo "zuletzt genutzte dateien wurden bereinigt"
-sudo bleachbit --clean --all-but-warning 2>/dev/null | grep Gewonnen
-bleachbit --clean --all-but-warning 2>/dev/null | grep Gewonnen
