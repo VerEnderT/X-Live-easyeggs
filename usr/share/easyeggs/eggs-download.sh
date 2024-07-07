4f712a92f4dade30e3df47301ea86be692002070
VerLinuxT <53666253+VerEnderT@users.noreply.github.com>
2024-07-07 08:43:31 +0200

    Update eggs-download.sh

--- usr/share/easyeggs/eggs-download.sh
+++ usr/share/easyeggs/eggs-download.sh
@@ -19,10 +19,3 @@ echo "penguins-eggs-latest_amd64.deb wird heruntergeladen"
 curl -L -o /tmp/penguins-eggs-latest_amd64.deb $DOWNLOAD_URL
 
 echo "Download abgeschlossen: penguins-eggs-latest_amd64.deb"
-
-
-echo "penguins-eggs-latest_amd64.deb wird installiert"
-
-sudo apt install /tmp/penguins-eggs-latest_amd64.deb -y
-
-sudo rm /tmp/penguins-eggs-latest_amd64.deb
